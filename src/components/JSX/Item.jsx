a48b92464c49c6c51ba994a1523cb63362eabf52
Alvaro Iriondo <alvaropereyra11@hotmail.com>
2024-11-16 19:23:51 -0300

    comm

--- src/components/JSX/Item.jsx
+++ src/components/JSX/Item.jsx
@@ -6,6 +6,8 @@ import '../CSS/Item.css';
 const Item = ({ item }) => {
   const alturaEnCm = item.height * 10; 
   const pesoEnKg = item.weight * 0.1; 
+    // Las medidas de la api estan en hectogramos y decimetros, por lo tanto para modificar estas medidas necesite de estos calculos, esto puede generar algunos problemas de redondeo como ahora mismo tiene Caterpie, Dugtrio y Golduck.//
+
   return (
     <div className="item">
       <img src={item.sprites.front_default} alt={item.name} className="item-image" />
